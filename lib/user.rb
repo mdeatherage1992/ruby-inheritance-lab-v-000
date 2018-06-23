d5f68fb541054181ad6f039e6f7336d352807f7b
Matthew Deatherage <mdeatherage1992@gmail.com>
2018-06-23 14:57:14 +0000

    automatically backed up by learn

--- lib/user.rb
+++ lib/user.rb
@@ -2,7 +2,7 @@ require 'pry'
 class User
   attr_accessor :first_name, :last_name
 
-  def initialize(firstname,lastname)
+  def initialize(first_name,last_name)
     @first_name = first_name
     @last_name = last_name
   end
