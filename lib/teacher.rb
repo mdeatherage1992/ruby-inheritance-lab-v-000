b7ce37e30a3aa00f40888ea6351f49035c81d8ba
Matthew Deatherage <mdeatherage1992@gmail.com>
2018-06-23 15:12:53 +0000

    automatically backed up by learn

--- lib/teacher.rb
+++ lib/teacher.rb
@@ -5,12 +5,12 @@ class Teacher < User
     @first_name = first_name
     @last_name = last_name
   end
+  KNOWLEDGE = ["a String is a type of data in Ruby", "programming is hard, but it's worth it", "javascript async web request", "Ruby method call definition", "object oriented dog cat class instance", "class method class variable instance method instance variable", "programming computers hacking learning terminal", "bash Ruby rvm update certs"]
 
   def teach
     sample(KNOWLEDGE)
   end
 
-KNOWLEDGE = ["a String is a type of data in Ruby", "programming is hard, but it's worth it", "javascript async web request", "Ruby method call definition", "object oriented dog cat class instance", "class method class variable instance method instance variable", "programming computers hacking learning terminal", "bash Ruby rvm update certs"]
 
 
 end
