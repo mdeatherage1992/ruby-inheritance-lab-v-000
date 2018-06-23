0e83f4abf07b7634d924e8bd538a80114c9fee4c
Matthew Deatherage <mdeatherage1992@gmail.com>
2018-06-23 15:23:36 +0000

    automatically backed up by learn

--- lib/teacher.rb
+++ lib/teacher.rb
@@ -2,14 +2,15 @@ require "pry"
 require_relative "./user.rb"
 class Teacher < User
   attr_accessor :first_name,:last_name
+
   def initialize
     @first_name = first_name
     @last_name = last_name
   end
 
-def teach
-  knowledge_string = ["a String is a type of data in Ruby", "programming is hard, but it's worth it", "javascript async web request", "Ruby method call definition", "object oriented dog cat class instance", "class method class variable instance method instance variable", "programming computers hacking learning terminal", "bash Ruby rvm update certs"]
-  sample(knowledge_string)
+def teach(KNOWLEDGE)
+  KNOWLEDGE = ["a String is a type of data in Ruby", "programming is hard, but it's worth it", "javascript async web request", "Ruby method call definition", "object oriented dog cat class instance", "class method class variable instance method instance variable", "programming computers hacking learning terminal", "bash Ruby rvm update certs"]
+  sample(KNOWLEDGE)
 end
 
 
