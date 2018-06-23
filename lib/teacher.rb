7b37504e84cd70e77251822aaab41262cb95f6ff
Matthew Deatherage <mdeatherage1992@gmail.com>
2018-06-23 15:21:11 +0000

    automatically backed up by learn

--- lib/teacher.rb
+++ lib/teacher.rb
@@ -9,7 +9,7 @@ class Teacher < User
 
 def teach
   knowledge_string = ["a String is a type of data in Ruby", "programming is hard, but it's worth it", "javascript async web request", "Ruby method call definition", "object oriented dog cat class instance", "class method class variable instance method instance variable", "programming computers hacking learning terminal", "bash Ruby rvm update certs"]
-  sample(KNOWLEDGE)
+  sample(knowledge_string)
 end
 
 
